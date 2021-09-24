748dedc158a2afe250f7bfceecf2d425e06a4d20
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2021-09-24 03:52:08 +0100

    erreur msg

--- resources/views/vendor/jetstream/components/validation-errors.blade.php
+++ resources/views/vendor/jetstream/components/validation-errors.blade.php
@@ -1,6 +1,6 @@
 @if ($errors->any())
     <div {!! $attributes->merge(['class' => 'alert alert-danger text-sm p-2']) !!} role="alert">
-        <div class="font-weight-bold">{{ __('Whoops! Something went wrong.') }}</div>
+        <div class="font-weight-bold">{{ __('Invalid Email or Password') }}</div>
 
         <ul>
             @foreach ($errors->all() as $error)
